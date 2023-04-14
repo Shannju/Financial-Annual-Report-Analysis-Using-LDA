f7f276af0f0fd1e2f71d2921001e5e011feea0b7
Shannju <Wangshan9102@163.com>
2023-04-15 00:45:42 +0800

    2.0

--- parsing.py
+++ parsing.py
@@ -16,25 +16,47 @@ parent_path = os.path.dirname(abs_path)
 
 txt_path = parent_path+r"\txt"
 # print("当前脚本的txt文件夹的绝对路径为：", txt_path)
+keywords_path = parent_path+r"\dictionary.txt"
+
+def clean_text(text):
+    # 使用正则表达式删除非法字符
+    cleaned_text = re.sub(r'[\x00-\x1F]+', '', text)
+    return cleaned_text
+
+
+def read_keywords(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        keywords = file.read().splitlines()
+    return keywords
+
+def contains_keyword(sentence, keywords):
+    for keyword in keywords:
+        if keyword in sentence:
+            return True
+    return False
+
+
+def read_and_split_file(file_path, keywords_path):
+    keywords = read_keywords(keywords_path)
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+        content = content.replace('\n', ',')
+        content = content.replace(' ', ',')
+        # 去除形如(cid:3311)的字符组合
+        content = re.sub(r'\(cid:\d+\)', '', content)
+        # 使用正则表达式分割文本，包含中文标点
+        sentences = re.split(r'[.,;!?。，；！？：]', content)
+        sentences = [
+            s.strip() for s in sentences
+            if s.strip() and is_valid_sentence(s.strip()) and contains_keyword(s.strip(), keywords)
+        ]
+    return sentences
 
 
 
 def is_valid_sentence(sentence):
     return bool(re.search(r'[\u4e00-\u9fa5]', sentence))
-
-def read_and_split_file(file_path):
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-        # 去除换行符
-        content = content.replace('\n', ' ')
-        # 去除形如(cid:3311)的字符组合
-        content = re.sub(r'\(cid:\d+\)', '', content)
-        # 使用正则表达式分割文本，包含中文标点
-        sentences = re.split(r'[.,;!?。，；！？：]', content)
-        # 去除前后空格并过滤无效句子
-        sentences = [s.strip() for s in sentences if s.strip() and is_valid_sentence(s.strip())]
-    return sentences
 
 def process_folder(folder_path):
     # print(f"path in function ",folder_path)
@@ -43,18 +65,50 @@ def process_folder(folder_path):
     for file_name in os.listdir(folder_path):
         if file_name.endswith('.txt'):
             file_path = os.path.join(folder_path, file_name)
-            sentences = read_and_split_file(file_path)
+            sentences = read_and_split_file(file_path,keywords_path)
             all_sentences.extend(sentences)
     return all_sentences
+
+import os
+import re
+import pandas as pd
+
+def process_folder(folder_path):
+    # 创建一个空的DataFrame，列名为'content', 'chinese', 'year'
+    df = pd.DataFrame(columns=['content', 'chinese', 'year'])
+
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.txt'):
+            file_path = os.path.join(folder_path, file_name)
+            sentences = read_and_split_file(file_path, keywords_path)
+
+            # 使用正则表达式提取中文字符和年份
+            chinese = ''.join(re.findall(r'[\u4e00-\u9fff]+', file_name))
+            year = ''.join(re.findall(r'\d{4}', file_name))
+
+            for sentence in sentences:
+                # 使用pandas.concat将每个句子、中文字符和年份添加到DataFrame中
+
+                new_row = pd.DataFrame({'content': [clean_text(sentence)], 'chinese': [chinese], 'year': [year]})
+                df = pd.concat([df, new_row], ignore_index=True)
+
+    # 将DataFrame保存为一个Excel文件
+    df.to_excel('output.xlsx', index=False)
+
+    return df
+
+
 
 def write_to_file(sentences_array, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
         for sentence in sentences_array:
-            file.write(sentence + '\n')
+            cleaned_sentence = clean_text(sentence)  # 清理句子中的非法字符
+            file.write(cleaned_sentence + '\n')
 
 
 
-sentences_array = process_folder(txt_path)
-# print(sentences_array)
-output_file = 'sentences.txt'
-write_to_file(sentences_array, output_file)
+# 调用process_folder函数，并将结果存储为一个DataFrame
+result_df = process_folder(txt_path)
+
+# 将DataFrame保存为一个Excel文件
+result_df.to_excel('output.xlsx', index=False)
